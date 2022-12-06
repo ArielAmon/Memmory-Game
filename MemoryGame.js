0c5e6cb880323fdfc85f34181fee080c8bea811b
Ariel Amon <arielamon1445@gmail.com>
2022-12-06 21:13:18 +0200

    start Working on the game

--- MemoryGame.js
+++ MemoryGame.js
@@ -1,19 +1,7 @@
 "use strict";
 (()=>{
-    /**
-     *
-     * @param playersList
-     * @returns {string}
-     */
-    function createTable (playersList) {
-        let playersTable = "<table class='table table-hover table-light table-striped-columns'>"+
-            " <thead >" + "<tr>" + " <th> Rank </th> " + " <th> Player </th> " + " <th> Score </th> " + "</tr> " + "</thead>" + "<tbody > " ;
-        playersList.forEach(player =>{
-            playersTable += "" + "<tr  > " + "<td >" + (playersList.indexOf(player) + 1) + " </td> " + "<td >" + player.player + " </td> " + "<td >" + player.score + " </td> " + " </tr> "
-        })
-        playersTable += "</tbody> " + "</table> " ;
-        return playersTable;
-    }
+
+    //-----------------------------------------------------------------------------------
 
     function getSetHtmlElements (){
 
@@ -42,10 +30,10 @@
         const cols = () =>{ return selectCols; }
         const delay = () =>{ return selectDelay; }
 
-        const rowsDivEelem = () =>{ return rowsDiv; }
-        const colsDivEelem = () =>{ return colsDiv; }
-        const nameDivEelem = () =>{ return NameDiv; }
-        const formDivEelem = () =>{ return formDiv; }
+        const rowsDivElem = () =>{ return rowsDiv; }
+        const colsDivElem = () =>{ return colsDiv; }
+        const nameDivElem = () =>{ return NameDiv; }
+        const formDivElem = () =>{ return formDiv; }
 
         const leaderboard = () =>{ return leaderboardModal; }
         const leaderboardContent = () =>{ return leaderboardModalContent; }
@@ -68,10 +56,10 @@
             getRowsSelect : rows,
             getColsSelect : cols,
             getDelaySelect : delay,
-            getRowsDivElem : rowsDivEelem,
-            getColsDivElem : colsDivEelem,
-            getNameDivElem : nameDivEelem,
-            getformDivElem : formDivEelem,
+            getRowsDivElem : rowsDivElem,
+            getColsDivElem : colsDivElem,
+            getNameDivElem : nameDivElem,
+            getFormDivElem : formDivElem,
             getPlayerNameElem : playerName,
             getLeaderboardModal : leaderboard,
             getLeaderboardContent : leaderboardContent,
@@ -82,7 +70,52 @@
 
     }
 
+//-----------------------------------------------------------------------------------
+
     function handleClickEvents (){
+
+        //private function
+        function createTable (playersList) {
+            let playersTable = "<table class='table table-hover table-light table-striped-columns'>"+
+                " <thead >" + "<tr>" + " <th> Rank </th> " + " <th> Player </th> " + " <th> Score </th> " + "</tr> " + "</thead>" + "<tbody > " ;
+            playersList.forEach(player =>{
+                playersTable += "" + "<tr  > " + "<td >" + (playersList.indexOf(player) + 1) + " </td> " + "<td >" + player.player + " </td> " + "<td >" + player.score + " </td> " + " </tr> "
+            })
+            playersTable += "</tbody> " + "</table> " ;
+            return playersTable;
+        }
+
+        //private function
+        function pickRandom (array, items)  {
+            const clonedArray = [...array];
+            const randomPicks = [];
+
+            for (let index = 0; index < items; index++) {
+                const randomIndex = Math.floor(Math.random() * clonedArray.length);
+
+                randomPicks.push(clonedArray[randomIndex]);
+                clonedArray.splice(randomIndex, 1);
+            }
+
+            return randomPicks;
+        }
+
+        //private function
+        function shuffle (array) {
+            const clonedArray = [...array];
+
+            for (let index = clonedArray.length - 1; index > 0; index--) {
+                const randomIndex = Math.floor(Math.random() * (index + 1));
+                const original = clonedArray[index];
+
+                clonedArray[index] = clonedArray[randomIndex];
+                clonedArray[randomIndex] = original;
+            }
+
+            return clonedArray;
+        }
+
+
 
         const leaderboardTable = (modal, content) =>{
             if (window.localStorage.length === 0){
@@ -101,19 +134,59 @@
             }
         }
 
+        const startGame = (images, rows, cols) => {
+            const picks = pickRandom(images, (rows * cols) / 2);
+            const items = shuffle([...picks, ...picks]);
+            console.log(items)
+            const table = document.querySelector('.board')
+
+            let index = 0
+            // create the table element
+            // create the rows and cells of the table
+            for (let row = 0; row < rows; row++) {
+                let tr = document.createElement("tr");
+                for (let col = 0; col < cols; col++,index++) {
+                    let td = document.createElement("td");
+                    let img = document.createElement("img");
+                    img.src = items[index].img;
+                    img.classList.add(`${row}${col}`)
+                    //img.classList.add(`${array[count++]}`)
+                    td.appendChild(img);
+                    tr.appendChild(td);
+                }
+                table.appendChild(tr);
+            }
+
+            // insert the table into the page
+
+
+            /*            for (let i = 0; i < items.length; i++) {
+                            const card = document.createElement('img')
+                            card.setAttribute('src', 'images/blank.png')
+                            card.setAttribute('data-id', i)
+                            //card.addEventListener('click', flipCard)
+
+                            grid.appendChild(card)
+                        }*/
+
+
+        }
+
         return {
-            displayHighScore : leaderboardTable
+            displayHighScore : leaderboardTable,
+            runGame : startGame
 
         }
     }
 
+    //-----------------------------------------------------------------------------------
     function validations (){
 
         const checkMatSize = (rows, cols) =>{
             return ((rows * cols) % 2) === 0;
         }
 
-        const checkSizeParamters = (data) => {
+        const checkSizeParameters = (data) => {
 
             if (!checkMatSize(data.row, data.col) && !document.contains(document.getElementById("wrongMatSizeMessage"))){
                 data.elemDiv.appendChild(data.addFunc("p","wrongMatSizeMessage",data.error,"block",`red`));
@@ -136,12 +209,14 @@
 
         return{
             matSizeValidator : checkMatSize,
-            validateGameBoardSize :checkSizeParamters,
+            validateGameBoardSize :checkSizeParameters,
             validateName : checkName
 
         }
 
     }
+    //-----------------------------------------------------------------------------------
+
 
     function getData() {
 
@@ -150,36 +225,53 @@
             "Name cannot contain special charcters, \n also be no more than 12 characters."
         ];
 
-        let playerName = "";
+        const unknownCard = { name: 'unknown', img: 'images/card.jpg'};
+        const cardArray = [
+            { name: 'Banana', img: 'images/0.jpg'},
+            { name: 'Grape', img: 'images/1.jpg'},
+            { name: 'Coconut', img: 'images/2.jpg'},
+            { name: 'Guava', img: 'images/3.jpg'},
+            { name: 'Watermelon', img: 'images/4.jpg'},
+            { name: 'Peach', img: 'images/5.jpg'},
+            { name: 'Blueberry', img: 'images/6.jpg'},
+            { name: 'Orange', img: 'images/7.jpg'},
+            { name: 'Apple', img: 'images/8.jpg'},
+            { name: 'Lemon', img: 'images/9.jpg'},
+            { name: 'Kiwi', img: 'images/10.jpg'},
+            { name: 'Raspberry', img: 'images/11.jpg'},
+            { name: 'Apricot', img: 'images/12.jpg'},
+            { name: 'Pineapple', img: 'images/13.jpg'},
+            { name: 'Strawberry', img: 'images/14.jpg'},
+            { name: 'Melon', img: 'images/15.jpg'},
+        ];
+
+        let playersList = [];
         let boardRows, boardCols, gameDelay ;
         boardRows = boardCols = gameDelay = 0;
-        let playersList = [];
 
         return{
-
-            getPlayerName: ()=>{ return playerName;},
             getBoardRows: ()=>{ return boardRows;},
             getBoardCols: ()=>{ return boardCols;},
             getGameDelay: ()=>{ return gameDelay;},
 
             getError: (index)=>{ return errorMessages[index];},
+            getImagesArray:()=>{return cardArray;},
+            getPlayersList :()=>{return playersList;},
+            getCardArray :()=>{return cardArray;},
 
-            setPlayerName: (name)=>{ playerName = name;},
             setBoardRows: (rows)=>{ boardRows = rows;},
             setBoardCols: (cols)=>{ boardCols = cols;},
             setGameDelay: (delay)=>{ gameDelay = delay;},
 
-            initGameStat: (name, rows, cols, delay)=>{
-                playersList.append(name)
-                playerName = name;
+            initGameStat: (name, rows, cols)=>{
+                playersList.push({name : 0})
                  boardRows = rows;
                  boardCols = cols;
-                 gameDelay = delay;
             },
 
         }
     }
-
+    //-----------------------------------------------------------------------------------
     document.addEventListener("DOMContentLoaded", () => {
 
         const domElements = getSetHtmlElements();
@@ -245,10 +337,11 @@
                 if (document.contains(document.getElementById("wrongName")))
                     document.getElementById("wrongName").remove()
 
-                domElements.getformDivElem().style.display = 'none';
+                domElements.getFormDivElem().style.display = 'none';
+                gameData.initGameStat(name, domElements.getRowsSelect().value, domElements.getColsSelect().value);
+                handleButtonsClick.runGame(gameData.getCardArray(),gameData.getBoardRows(),gameData.getBoardCols());
+                document.getElementById("game").style.display = 'block'
 
-                //gameData.initGameStat()
-                // run game
             }
             else{
                if (!document.contains(document.getElementById("wrongName")))
